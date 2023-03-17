1b464ce2308a95ffb01fea7bbe5cd7572cb3a6b2
cz160 <1604506698@qq.com>
2023-03-17 16:20:07 +0800

    feat: 构建流程

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,36 +1,35 @@
-import { defineConfig } from 'astro/config'
-import unocss from 'unocss/astro'
-import solidJs from '@astrojs/solid-js'
-import vercelDisableBlocks from './plugins/vercelDisableBlocks'
+import { defineConfig } from "astro/config";
+import unocss from "unocss/astro";
+import solidJs from "@astrojs/solid-js";
+import vercelDisableBlocks from "./plugins/vercelDisableBlocks";
 
-import node from '@astrojs/node'
-import vercel from '@astrojs/vercel/edge'
+import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/edge";
 import netlify from "@astrojs/netlify/edge-functions";
 
 const envAdapter = () => {
-  if (process.env.OUTPUT == 'vercel') {
-    return vercel()
-  } else if (process.env.OUTPUT == 'netlify') {
-    return netlify()
+  if (process.env.OUTPUT == "vercel") {
+    return vercel();
+  } else if (process.env.OUTPUT == "netlify") {
+    return netlify();
   } else {
     return node({
-      mode: 'standalone'
-    })
+      mode: "standalone",
+    });
   }
-}
+};
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    unocss(),
-    solidJs()
-  ],
-  output: 'server',
+  site: "https://cz160.github.io",
+  base: "/cz-chatgpt",
+  integrations: [unocss(), solidJs()],
+  output: "server",
   adapter: envAdapter(),
   vite: {
     plugins: [
-      process.env.OUTPUT == 'vercel' && vercelDisableBlocks(),
-      process.env.OUTPUT == 'netlify' && vercelDisableBlocks(),
-    ]
+      process.env.OUTPUT == "vercel" && vercelDisableBlocks(),
+      process.env.OUTPUT == "netlify" && vercelDisableBlocks(),
+    ],
   },
 });
